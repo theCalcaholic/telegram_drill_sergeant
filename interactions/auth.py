184917f307a8d84042b45eaa7eb4a560e6e53fbc
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-04-26 16:37:40 +0200

    Add counters to authorization dialog

--- interactions/auth.py
+++ interactions/auth.py
@@ -6,24 +6,49 @@ from typing import Any, Callable, Optional
 import random
 import string
 
+authorization_dialog_text = 'Use the buttons below to de-/authorize or register yourself to be included in this ' \
+                            'group\'s statistics\n\n' \
+                            'authorization count: {}\n' \
+                            'users registered: {}'
+
+
+def authorized(func_or_result: Optional = None, *args):
+    def inner(func: Callable[[Update, CallbackContext], Any]):
+        def wrapped(update: Update, context: CallbackContext):
+            uid = update.effective_user.id
+            if uid not in context.bot_data['users'] or not context.bot_data['users'][uid].authorized:
+                print(f'User {uid} is not authorized!')
+                update.effective_chat.send_message("Sorry, you're not authorized to use this bot.")
+                return func_or_result
+            return func(update, context)
+
+        return wrapped
+
+    if isinstance(func_or_result, Callable):
+        return inner(func_or_result)
+    else:
+        return inner
+
 
 def authorize_user(update: Update, context: CallbackContext):
     query = update.callback_query
     _, secret, action = query.data.split(':')
-    if 'auth_secrets' not in context.bot_data or secret not in context.bot_data['auth_secrets']:
+    if 'auth_secrets' not in context.chat_data or secret not in context.chat_data['auth_secrets']:
         query.answer('Your request was not authorized!')
         return
     uid = query.from_user.id
+
     if action == 'authorize':
         if uid in context.bot_data['users'] and context.bot_data['users'][uid].authorized:
             context.bot_data['users'][uid].authorized = False
             query.answer('Your authorization has been revoked!')
+            context.chat_data[secret + '_authcount'] -= 1
         else:
             if query.from_user.id not in context.bot_data['users']:
                 context.bot_data['users'].append(User(uid))
             context.bot_data['users'][uid].authorized = True
             query.answer('You have been authorized for using the Drill Sergeant!')
-        return
+            context.chat_data[secret + '_authcount'] += 1
     elif action == 'group_registration':
         if 'users' not in context.chat_data:
             context.chat_data['users'] = set()
@@ -33,32 +58,47 @@ def authorize_user(update: Update, context: CallbackContext):
         else:
             context.chat_data['users'].add(uid)
             query.answer('Your goals will now be included in this group\'s stats!')
-        return
-    elif action == 'close':
+
+    group_user_count = len(context.chat_data['users'])
+    auth_count = context.chat_data[secret + '_authcount']
+
+    if action == 'close':
         if uid not in context.bot_data['users'] or not context.bot_data['users'][uid].authorized:
-            query.answer('You have no permission to close this poll')
+            query.answer('You don\'t have permission to close this poll')
             return
-        context.bot_data['auth_secrets'].remove(secret)
+        context.chat_data['auth_secrets'].remove(secret)
+        del context.chat_data[secret + '_authcount']
         query.answer('authorization dialog closed!')
-        query.edit_message_text(query.message.text + '\n\n--closed--')
+
+    msg_text = authorization_dialog_text.format(auth_count, group_user_count) \
+               + ('\n\n--closed--' if action == 'close' else '')
+    if msg_text != query.message.text:
+        query.edit_message_text(msg_text,
+                                reply_markup=query.message.reply_markup if action != 'close' else None)
 
 
 @chat_types('group', 'supergroup')
+@authorized
 def show_auth_dialog(update: Update, context: CallbackContext):
     secret = ''.join(random.choice(string.ascii_letters) for _ in range(24))
-    if 'auth_secrets' not in context.bot_data:
-        context.bot_data['auth_secrets'] = set()
-    context.bot_data['auth_secrets'].add(secret)
+    for key in ('auth_secrets', 'users'):
+        if key not in context.chat_data:
+            context.chat_data[key] = set()
+    context.chat_data[secret + '_authcount'] = 0
+    context.chat_data['auth_secrets'].add(secret)
     option_template = f'authorization_dialog:{secret}:{{}}'
     keyboard = [[
         InlineKeyboardButton('de-/authorize me', callback_data=option_template.format('authorize')),
-        InlineKeyboardButton('register me for this group', callback_data=option_template.format('group_registration'))
+        InlineKeyboardButton('un-/register me for this group',
+                             callback_data=option_template.format('group_registration'))
     ], [
         InlineKeyboardButton('Close Dialog', callback_data=option_template.format('close'))
     ]]
-    print(secret)
-    update.message.reply_text('Use the buttons below to de-/authorize or register yourself to be included in this '
-                              'group\'s statistics', reply_markup=InlineKeyboardMarkup(keyboard))
+
+    group_user_count = len(context.chat_data['users'])
+    auth_count = context.chat_data[secret + '_authcount']
+    update.message.reply_text(authorization_dialog_text.format(auth_count, group_user_count),
+                              reply_markup=InlineKeyboardMarkup(keyboard))
 
 
 def authorize_user_old(update: Update, context: CallbackContext):
@@ -111,20 +151,3 @@ def auth_poll(update: Update, context: CallbackContext):
     }
 
     context.bot_data.update(payload)
-
-
-def authorized(func_or_result: Optional = None, *args):
-    def inner(func: Callable[[Update, CallbackContext], Any]):
-        def wrapped(update: Update, context: CallbackContext):
-            uid = update.effective_user.id
-            if uid not in context.bot_data['users'] or not context.bot_data['users'][uid].authorized:
-                print(f'User {uid} is not authorized!')
-                update.effective_chat.send_message("Sorry, you're not authorized to use this bot.")
-                return func_or_result
-            return func(update, context)
-
-        return wrapped
-    if isinstance(func_or_result, Callable):
-        return inner(func_or_result)
-    else:
-        return inner
