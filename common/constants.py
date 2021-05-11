1d2f7b077725269a81339e5e30d82164fe943253
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-05-11 13:01:07 +0200

    Add < to markdown special characters

--- common/constants.py
+++ common/constants.py
@@ -13,7 +13,7 @@ for t in goal_score_types:
 cron_pattern = re.compile(r'(?P<minute>(\d+|\*|)(/\d+)?)( *(?P<hour>(\d+|\*|)(/\d+)?))( *(?P<dom>(\d+|\*|)(/\d+)?))'
                           r'( *(?P<month>(\d+|\*|)(/\d+)?))( *(?P<dow>(\d+|\*|)(/\d+)?))')
 days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Sat']
-telegram_markdown_special_chars = '_*[]()~`>#+-=|{}.!'
+telegram_markdown_special_chars = '_*[]()~`<>#+-=|{}.!'
 
 reaction_stickers = {
     'approval': [
